c47e4282f9921e939b48b55051e56ef163e36331
deanhigh <deanhigh@users.noreply.github.com>
2025-05-17 16:32:54 +0100

    feat: ignore vim global

--- nvim/init.lua
+++ nvim/init.lua
@@ -48,5 +48,19 @@ require("lazy").setup({
   },
 })
 
-vim.lsp.enable('luals')
-vim.lsp.enable('gopls')
+vim.lsp.enable({'luals','gopls'})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.diagnostic.config({
+  virtual_text = { current_line = true }
+})
+
